31644559468240f1198e3559cc4c0aee252cdc8b
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2019-12-27 11:42:36 +0900

    check ping response from actuator after reboot

--- include/layered_hardware_dynamixel/reboot_mode.hpp
+++ include/layered_hardware_dynamixel/reboot_mode.hpp
@@ -15,11 +15,29 @@ public:
   RebootMode(const DynamixelActuatorDataPtr &data) : OperatingModeBase("reboot", data) {}
 
   virtual void starting() {
+    // instruct rebooting
     if (!data_->dxl_wb->reboot(data_->id)) {
       ROS_ERROR_STREAM("RebootMode::starting(): Failed to reboot the actuator '"
-                       << data_->name << "' (id: " << data_->id << ")");
+                       << data_->name << "' (id: " << static_cast< int >(data_->id) << ")");
+      return;
     }
-    // TODO: ping to wait rebooted ??
+    // wait rebooted
+    const ros::Time reboot_time(ros::Time::now());
+    while (true) {
+      // break on successful ping
+      if (data_->dxl_wb->ping(data_->id)) {
+        ROS_INFO_STREAM("RebootMode::starting(): Actuator '"
+                        << data_->name << "' (id: " << static_cast< int >(data_->id)
+                        << ") was successfully rebooted");
+        return;
+      }
+      // or timeout
+      if ((ros::Time::now() - reboot_time).toSec() > 0.5) {
+        ROS_ERROR_STREAM("RebootMode::starting(): No ping response after reboot from the actuator '"
+                         << data_->name << "' (id: " << static_cast< int >(data_->id) << ")");
+        return;
+      }
+    }
   }
 
   virtual void read(const ros::Time &time, const ros::Duration &period) {
