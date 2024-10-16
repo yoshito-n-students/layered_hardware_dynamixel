8b36d6a72c18c45858b00552db125ef6ea0fb9e0
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2024-10-16 13:00:06 +0000

    simply use vector instead of map

--- include/layered_hardware_dynamixel/dynamixel_actuator_layer.hpp
+++ include/layered_hardware_dynamixel/dynamixel_actuator_layer.hpp
@@ -1,7 +1,6 @@
 #ifndef LAYERED_HARDWARE_DYNAMIXEL_DYNAMIXEL_ACTUATOR_LAYER_HPP
 #define LAYERED_HARDWARE_DYNAMIXEL_DYNAMIXEL_ACTUATOR_LAYER_HPP
 
-#include <map>
 #include <memory>
 #include <string>
 #include <utility> // for std::move()
@@ -47,13 +46,15 @@ public:
     // parse parameters for this layer as yaml
     std::string serial_iface;
     std::uint32_t baudrate;
-    std::map<std::string, YAML::Node> actuator_list;
+    std::vector<std::string> ator_names;
+    std::vector<YAML::Node> ator_params;
     try {
       const YAML::Node params = YAML::Load(params_it->second);
       serial_iface = params["serial_interface"].as<std::string>("/dev/ttyUSB0");
       baudrate = params["baudrate"].as<int>(115200);
       for (const auto &name_param_pair : params["actuators"]) {
-        actuator_list.emplace(name_param_pair.first.as<std::string>(), name_param_pair.second);
+        ator_names.emplace_back(name_param_pair.first.as<std::string>());
+        ator_params.emplace_back(name_param_pair.second);
       }
     } catch (const YAML::Exception &error) {
       LHD_ERROR("DynamixelActuatorLayer::on_init(): %s (on parsing \"%s\" parameter)", //
@@ -64,19 +65,22 @@ public:
     // open USB serial device
     const auto dxl_wb = std::make_shared<DynamixelWorkbench>();
     if (!dxl_wb->init(serial_iface.c_str(), baudrate)) {
-      LHD_ERROR("DynamixelActuatorLayer::on_init(): Failed to open DynamielWorkbench");
+      LHD_ERROR("DynamixelActuatorLayer::on_init(): Failed to open DynamielWorkbench (%s, %d)",
+                serial_iface.c_str(), baudrate);
       return CallbackReturn::ERROR;
     }
 
     // init actuators with param "actuators/<actuator_name>"
-    for (const auto &[ator_name, ator_params] : actuator_list) {
+    for (std::size_t i = 0; i < ator_names.size(); ++i) {
       try {
-        actuators_.emplace_back(new DynamixelActuator(ator_name, ator_params, dxl_wb));
+        actuators_.emplace_back(new DynamixelActuator(ator_names[i], ator_params[i], dxl_wb));
       } catch (const std::runtime_error &error) {
+        LHD_ERROR("DynamixelActuatorLayer::init(): Failed to create driver for \"%s\" actuator",
+                  ator_names[i].c_str());
         return CallbackReturn::ERROR;
       }
       LHD_INFO("DynamixelActuatorLayer::init(): Initialized the actuator \"%s\"",
-               ator_name.c_str());
+               ator_names[i].c_str());
     }
 
     return CallbackReturn::SUCCESS;
